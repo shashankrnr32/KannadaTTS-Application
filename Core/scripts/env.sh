f3f258d210d7ce1be25d79e657172ca9e3a7f745
Shashank Sharma <shashankrnr32@gmail.com>
2019-04-07 09:29:12 +0530

    2.13 9.29_4.7

---
+++ Core/scripts/env.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# ===========================================================
+# Set Environment Variables
+# Dev : Shashank Sharma
+# ===========================================================
+
+cwd=$(pwd)
+export ESTDIR="$cwd/speech_tools"
+export FESTVOXDIR="$cwd/festvox"
+export SPTKDIR="$cwd/sptk"
+export VOICEDIR="$cwd/cmu_indic_kan_female"
+export WAVDIR="$VOICEDIR/test/tts"
+
+
--- Core/scripts/run_kss.sh
+++ Core/scripts/run_kss.sh
@@ -15,21 +15,8 @@ export WAVDIR="$VOICEDIR/test/tts"
 
 echo Changing to Voice Directory...
 cd $VOICEDIR
-
-if [ $# -eq 0 ];
-then
-	echo Using Default Test File...
-	$1=etc/txt.done.data.test
-fi
-
-echo Checking \for Test File...
-if [ -f $1 ];
-then
-	echo Test File Found...
-	echo Synthesing...
-	./bin/do_clustergen cg_test tts tts $1 > kss.log 2>&1
-fi
-
+echo Synthesing...
+./bin/do_clustergen cg_test tts tts $1 > kss.log 2>&1
 echo "Successfully Synthesized. 
 Check the Audio Files in $WAVDIR"
 
